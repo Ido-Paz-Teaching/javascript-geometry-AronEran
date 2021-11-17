dad6bfe61be66f73eb815b2874a51f7e9b52e9c3
AronEran <aroneran@example.com>
2021-11-17 16:01:36 +0200

    Update

--- geometry.js
+++ geometry.js
@@ -1,13 +1,17 @@
 function getTriangleArea(height, width) {
   /* Your amazing code here 🚀 */
+  return height * width / 2;
 }
 
 function getSquareArea(side) {
   /* Your amazing code here 🚀 */
+  return Math.pow(side,2);
 }
 
 function getCircleArea(radius) {
   /* Your amazing code here 🚀 */
+  let s = (Math.PI).toFixed(2) * Math.pow(radius,2);
+  return s.toFixed(2)
 }
 
 //Please , don't remove the following code 
